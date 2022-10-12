0f2623246e0829de7b6ec69dca7e963775c34010
Nadezhda Khovrich <hope28281@gmail.com>
2022-10-13 00:11:00 +0600

    Добавлен поиск в TaskListView

--- issue/forms.py
+++ issue/forms.py
@@ -1,8 +1,12 @@
 from issue.models import Task
-from django.forms import ModelForm
+from django import forms
 
 
-class TaskForm(ModelForm):
+class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['summary', 'description',  'status',  'type']
+
+
+class SearchTaskForm(forms.Form):
+    search = forms.CharField(required=False)
--- issue/views.py
+++ issue/views.py
@@ -1,9 +1,9 @@
-from multiprocessing import context
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import TemplateView, ListView
 from typing import Any
-from issue.forms import TaskForm
+from issue.forms import TaskForm, SearchTaskForm
 from issue.models import Task
+from django.db.models import Q
 
 
 class TaskListView(ListView):
@@ -13,11 +13,42 @@ class TaskListView(ListView):
     paginate_by = 3
     paginate_orphans = 1
 
+ 
+    def get_context_data(self, **kwargs):
+        print('get_context_data')
 
-    # def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
-    #     context = super().get_context_data(**kwargs)
-    #     context['tasks'] = Task.objects.all()
-    #     return context
+        context = super().get_context_data(**kwargs)
+        context['form'] = self.form
+        return context
+
+    def get(self, request, *args, **kwargs):
+        print('get')
+
+        self.form = self.get_search_form()
+        self.search_value = self.get_search_value()
+        print(self.search_value)
+        return super().get(request, *args, **kwargs)
+
+    def get_search_form(self):
+        print('get_search_form')
+        return SearchTaskForm(self.request.GET)
+
+    def get_search_value(self):
+        print('get_search_value')
+
+        if self.form.is_valid():
+            return self.form.cleaned_data.get('search')
+        return None
+
+    def get_queryset(self):
+        print('get_queryset')
+        queryset = super().get_queryset()
+        if self.search_value:
+            queryset = queryset.filter(
+                Q(summary__iregex=self.search_value) | Q(description__iregex=self.search_value)
+                )
+        return queryset
+
 
 class TaskDetailView(TemplateView):
     template_name: str = 'task_detail.html'
