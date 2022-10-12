2c3d3cc14e3dc1946c54adeddcd22f74c6944ea0
Nadezhda Khovrich <hope28281@gmail.com>
2022-10-13 00:58:24 +0600

    Создана модель Project, миграции

--- issue/admin.py
+++ issue/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from issue.models import Status, Task, Type
+from issue.models import Status, Task, Type, Project
 
 admin.site.register(Status)
 admin.site.register(Task)
 admin.site.register(Type)
+admin.site.register(Project)
--- issue/models.py
+++ issue/models.py
@@ -15,6 +15,17 @@ class Type(models.Model):
         return self.name
 
 
+class Project(models.Model):
+    name = models.CharField(verbose_name='Название проекта', max_length=50, null=False)
+    description = models.TextField(verbose_name='Описание', null=True)
+    start_date = models.DateField(verbose_name='Дата начала', null=False)
+    end_date = models.DateField(verbose_name='Дата окончания', null=True)
+
+
+    def __str__(self):
+        return self.name
+
+
 class Task(models.Model):
     summary = models.CharField(verbose_name='Заголовок', max_length=200, null=False)
     description = models.TextField(verbose_name='Описание', null=True)
@@ -22,6 +33,8 @@ class Task(models.Model):
     type = models.ForeignKey(to='issue.Type', verbose_name='Тип', related_name='type', on_delete=models.RESTRICT)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
+    project = models.ForeignKey(to='issue.Project', verbose_name='Проект', related_name='project', on_delete=models.RESTRICT)
 
     def __str__(self) -> str:
         return self.summary
+
